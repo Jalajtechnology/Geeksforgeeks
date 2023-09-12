69a783592ca067ce215901da1614a14a548d84a0
Jalajtechnology <141317612+Jalajtechnology@users.noreply.github.com>
2023-09-12 15:55:44 +0530

    Added Solution - GfG to GitHub

---
+++ School/Armstrong Numbers/armstrong-numbers.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string armstrongNumber(int n){
+        // code here
+        int sum=0;
+        int d=n;
+        while(n!=0)
+        {
+            int q=n%10;
+            sum=sum+(q*q*q);
+            n=n/10;
+        }
+        if(sum==d)
+        {
+            return "Yes";
+        }
+        return "No";
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.armstrongNumber(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
