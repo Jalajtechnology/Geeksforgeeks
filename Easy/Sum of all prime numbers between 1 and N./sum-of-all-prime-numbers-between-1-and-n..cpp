3606e8bccdf2716b3e3dadfa29438a0a07d1a078
Jalajtechnology <141317612+Jalajtechnology@users.noreply.github.com>
2023-09-14 21:23:58 +0530

    Update sum-of-all-prime-numbers-between-1-and-n..cpp

--- Easy/Sum of all prime numbers between 1 and N./sum-of-all-prime-numbers-between-1-and-n..cpp
+++ Easy/Sum of all prime numbers between 1 and N./sum-of-all-prime-numbers-between-1-and-n..cpp
@@ -1,5 +1,12 @@
 //{ Driver Code Starts
 //Initial Template for C++
+// Time complexity is n(log(logn)) used sieve algorithm  if n=18
+// for i=2  n/2 times inner loop will run for i=3 inner loop will run n/3 i=n ----n/ntimes
+//Tc :n/2+n/3+n/4+------------n/n
+//n(1/2+1/3+1/4+------1)
+//nlogn without condition arr[i]==1 with condition n(log(log(n)))
+//space complexity O(1)
+//Sieve algorithm is used .
 
 #include<bits/stdc++.h>
 using namespace std;
