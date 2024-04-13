7ab5ef5b505fd37d079a2e745aae85953d1a5c66
Jalaj Agarwal <141317612+Jalajtechnology@users.noreply.github.com>
2024-04-13 21:57:18 +0530

    Updated Solution - GfG to GitHub

--- Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
+++ Medium/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -13,40 +13,42 @@ class Solution{
     int maxLen(vector<int>&A, int n)
     {   
         // Your code here
-        int sum=0;
-        int max=INT_MIN;
         unordered_map<int,int>hashtable;
+        int max=0;;
+        int length=0;
+        int sum=0;
         for(int i=0;i<n;i++)
         {
             sum=sum+A[i];
             if(sum==0)
             {
-                max=i+1;
-                
-            }
-            else if(sum!=0)
-            {
-                if(hashtable.find(sum)!=hashtable.end())
+                length=i+1;
+                if(length>max)
                 {
-                    int length=i-hashtable[sum];
-                    if(max<length)
+                    max=length;
+                }
+            }
+            else if(sum !=0)
+            {
+                if(hashtable.find(sum-0)!=hashtable.end())
+                {
+                    length=i-hashtable[sum-0];
+                    if(length>max)
                     {
                         max=length;
                     }
                 }
-                else if(hashtable.find(sum)==hashtable.end())
-                {
-                    hashtable[sum]=i;
-                }
+            }
+            if(hashtable.find(sum)==hashtable.end())
+            {
+                hashtable[sum]=i;
             }
             
-            
         }
-        if(max==INT_MIN)
-        {
-            return 0;
-        }
+        
         return max;
+        
+        
     }
 };
 
